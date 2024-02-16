c8dd9b4ebcb2f24380978766f74b7c77e8cdc4da
var500 <varun5chodha@gmail.com>
2024-02-17 04:00:18 +0530

    Add: Contacts Page UI

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,25 @@
+import { MapPin } from "lucide-react";
+import Image, { StaticImageData } from "next/image";
+import React from "react";
+
+export default function ContactCard({
+  icon,
+  heading,
+  content,
+  span,
+}: {
+  icon: React.ReactNode;
+  heading: string;
+  content: string;
+  span: string;
+}) {
+  return (
+    <div
+      className={`bg-white h-52 flex flex-col items-center py-10 px-10 justify-between col-span-${span} shadow-md`}
+    >
+      {icon}
+      <p className="text-xl font-bold text-[#777777]">{heading}</p>
+      <p className="text-sm font-normal text-black">{content}</p>
+    </div>
+  );
+}
---
+++ src/components/Path.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Typography } from '@material-tailwind/react'
+import Link from 'next/link';
+import React from 'react'
+
+export default function Path({tab}:{tab:string}) {
+  return (
+    <div className='flex flex-wrap rounded-lg border-dashed h-5 px-4 border-2 bg-white my-4  items-center'>
+      <Typography placeholder={ undefined } className='text-xs'>
+        <Link href={ '/' } className='text-blue-700 mr-1 hover:text-orange-900'>Home</Link>
+        <span className='capitalize'>{`/ ${tab}`}</span>
+      </Typography>
+    </div>
+  )
+}
