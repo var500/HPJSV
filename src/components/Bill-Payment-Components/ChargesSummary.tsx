54b63a5b55790bcf95de6ece56cd200273881a67
var500 <varun5chodha@gmail.com>
2024-02-18 03:16:22 +0530

    Preetier formatting

--- src/components/Bill-Payment-Components/ChargesSummary.tsx
+++ src/components/Bill-Payment-Components/ChargesSummary.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { BillingData } from "@/utils/types";
 
-export default function ChargesSummary({data}:{data:BillingData}) {
-  const prevBalance = parseFloat(data?.chargesSummary?.prevBalance).toFixed(2) || "0.00";
-  const currentWaterCharges = parseFloat(data.Usage.netReadingM3)*13.86; 
-  const swerageCharges = currentWaterCharges*0.30; 
-  const totalPayable = Math.round(currentWaterCharges+swerageCharges);
-  const amountAfterDueDate = totalPayable*0.10;
+export default function ChargesSummary({ data }: { data: BillingData }) {
+  const prevBalance =
+    parseFloat(data?.chargesSummary?.prevBalance).toFixed(2) || "0.00";
+  const currentWaterCharges = parseFloat(data.Usage.netReadingM3) * 13.86;
+  const swerageCharges = currentWaterCharges * 0.3;
+  const totalPayable = Math.round(currentWaterCharges + swerageCharges);
+
+  const amountAfterDueDate = totalPayable * 0.1;
   return (
     <div className="max-w-2xl">
-      <div className="text-base md:text-xl font-bold mb-2 md:mb-4">Charges Summary</div>
+      <div className="text-base md:text-xl font-bold mb-2 md:mb-4">
+        Charges Summary
+      </div>
       <Table>
         <TableBody className="border-2 text-xs md:text-base">
           <TableRow>
@@ -21,27 +25,37 @@ export default function ChargesSummary({data}:{data:BillingData}) {
             <TableCell className="font-medium">
               Current Water Charges:
             </TableCell>
-            <TableCell className="text-right">{currentWaterCharges.toFixed(2)}</TableCell>
+            <TableCell className="text-right">
+              {currentWaterCharges.toFixed(2)}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell className="font-medium">Swerage Charges:</TableCell>
-            <TableCell className="text-right">{swerageCharges.toFixed(2)}</TableCell>
+            <TableCell className="text-right">
+              {swerageCharges.toFixed(2)}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell className="font-medium">Total Amount Payble:</TableCell>
-            <TableCell className="text-right">{totalPayable.toFixed(2)}</TableCell>
+            <TableCell className="text-right">
+              {totalPayable.toFixed(2)}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell className="font-medium">
               Surcharge Amount After Due Date:
             </TableCell>
-            <TableCell className="text-right">{amountAfterDueDate.toFixed(2)}</TableCell>
+            <TableCell className="text-right">
+              {amountAfterDueDate.toFixed(2)}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell className="font-medium">
               Total Amount payable After Due Date:
             </TableCell>
-            <TableCell className="text-right">{Math.round(amountAfterDueDate+totalPayable).toFixed(2)}</TableCell>
+            <TableCell className="text-right">
+              {Math.round(amountAfterDueDate + totalPayable).toFixed(2)}
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
