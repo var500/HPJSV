acda51172e5d096ccd0cf932d38478f8163654a4
var500 <varun5chodha@gmail.com>
2024-02-18 03:14:50 +0530

    Define Constants

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,31 +1,50 @@
 import { BillingData } from "./types";
 
 export const initial: BillingData = {
-    id: "",
-    Consumer: {
-      name: "",
-      address1: "",
-      ward: "",
-      contactNumber: "",
-    },
-    AccountDetails: {
-      meterNo: "",
-      accountNo: "",
-    },
-    Usage: {
-      oldReading: "",
-      newReading: "",
-      netReadingM3: "",
-      netReadingLtr: "",
-    },
-    chargesSummary: {
-      prevBalance: "",
-    },
-    BillingInfo: {
-      billNo: "",
-      billDate: "",
-      dueDate: "",
-      billMonth: "",
-      sewerage: "",
-    },
-  };
+  id: "",
+  Consumer: {
+    name: "",
+    address1: "",
+    ward: "",
+    contactNumber: "",
+  },
+  AccountDetails: {
+    meterNo: "",
+    accountNo: "",
+  },
+  Usage: {
+    oldReading: "",
+    newReading: "",
+    netReadingM3: "",
+    netReadingLtr: "",
+  },
+  chargesSummary: {
+    prevBalance: "",
+  },
+  BillingInfo: {
+    billNo: "",
+    billDate: "",
+    dueDate: "",
+    billMonth: "",
+    sewerage: "",
+  },
+};
+
+export const getRazorpayKey = () => {
+  const Razorpay_Key = process.env.NEXT_PUBLIC_RAZORPAY_KEY;
+
+  if (!Razorpay_Key) {
+    throw new Error("ENV variables not found");
+  }
+
+  return Razorpay_Key;
+};
+
+export const getRazorpaySecret = () => {
+  const Razorpay_Secret = process.env.NEXT_PUBLIC_RAZORPAY_SECRET;
+  if (!Razorpay_Secret) {
+    throw new Error("ENV variables not found");
+  }
+
+  return Razorpay_Secret;
+};
