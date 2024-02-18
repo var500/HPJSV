d0c38f495aa544fe94582a1d7430c390f2bc1e69
var500 <varun5chodha@gmail.com>
2024-02-18 20:47:24 +0530

    Add: Ticker

---
+++ src/components/Home-Components/Heading.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Title from "./Title";
+
+export default function Heading() {
+  return (
+    <div className="flex flex-col justify-center pb-5 mt-5 gap-5 px-8">
+      <Title />
+      <p className="text-justify text-base font-normal">
+        This web portal has been developed as a complete ERP solution to not
+        only automate the functioning of the entire Department, but also to
+        provide the decision makers with sufficient online information to
+        provide better services to citizens through proper and timely execution
+        of water supply and irrigation schemes in the State of Himachal Pradesh.
+        The software is work-flow based and is capable of generating all
+        accounting and inventory reports in real time mode. The software takes
+        base data of schemes from the State Budget database and the planners are
+        able to monitor these schemes. This software is also supplemented with a
+        no. of mobile apps. This web portal also facilitates the department to
+        generate the water bills of consumers, thereby facilitating consumers to
+        pay their bills in online mode. The consumers can also apply for new
+        water connections online, using this portal.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Home-Components/Title.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div className="flex flex-col items-center">
+        <p className="font-bold pb-5 text-[28px] font-sans text-[#444444]">
+          जल शक्ति विभाग
+        </p>
+        <p className="border-b-[3px] border-[#4a84db] w-10" />
+      </div>
+  )
+}
