c11032dd205838a25755433e56ba9b121d302949
var500 <varun5chodha@gmail.com>
2024-02-19 03:20:36 +0530

    Fix: Font-weight

--- src/components/Common/Path.tsx
+++ src/components/Common/Path.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 export default function Path({tab}:{tab:string}) {
   return (
     <div className='flex flex-wrap rounded-lg border-dashed h-6 px-4 border-2 bg-white my-4  items-center'>
-      <Typography placeholder={ undefined } className='text-sm font-extralight'>
+      <Typography placeholder={ undefined } className='text-sm font-light'>
         <Link href={ '/' } className='text-blue-700 mr-1 hover:text-orange-900'>Home</Link>
         <span className='capitalize'>{`/ ${tab}`}</span>
       </Typography>
