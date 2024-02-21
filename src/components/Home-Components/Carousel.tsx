4207f4a44aaf1176e01b1c5a45886898a43f9272
var500 <varun5chodha@gmail.com>
2024-02-21 15:09:08 +0530

    fix: Carousel Responsiveness

--- src/components/Home-Components/Carousel.tsx
+++ src/components/Home-Components/Carousel.tsx
@@ -6,25 +6,25 @@ import img2 from "@/assets/img2.png";
 import img3 from "@/assets/img3.png";
 export function CarouselDefault() {
   return (
-    <Carousel className="max-h-64 lg:max-h-96 overflow-hidden" placeholder={"Carousel"} autoplay loop>
+    <Carousel className="max-h-40 lg:max-h-96 overflow-hidden" placeholder={"Carousel"} autoplay loop>
       <Image
         src={img1}
         alt="image 1"
-        className="object-fill w-full h-64 lg:h-96"
+        className="object-cover md:object-fill w-full h-40 lg:h-96"
         height={530}
         width={1524}
       />
       <Image
         src={img3}
         alt="image 3"
-        className="object-fill w-full h-64 lg:h-96"
+        className="object-cover md:object-fill w-full h-40 lg:h-96"
         height={530}
         width={1524}
       />
       <Image
         src={img2}
         alt="image 2"
-        className="object-fill w-full h-64 lg:h-96"
+        className="object-cover md:object-fill w-full h-40 lg:h-96"
         height={530}
         width={1524}
       />
